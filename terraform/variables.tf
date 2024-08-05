d75307b7e6b9cab9caee137c2520eab54ca2d724
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2024-08-05 11:04:49 -0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,9 @@
-sfsfs
+variable "account_id" {
+  description = "The ID of the account."
+  type        = string
+}
+
+variable "PAT" {
+  description = "The Platform API key."
+  type        = string
+}
