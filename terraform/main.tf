9de3b7e7443b71afde2472b3e2ee0db07baff34a
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2024-08-05 11:05:28 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,15 +6,6 @@ terraform {
     }
   }
 }
-variable "account_id" {
-  description = "The ID of the account."
-  type        = string
-}
-
-variable "PAT" {
-  description = "The Platform API key."
-  type        = string
-}
 
 provider "harness" {
   endpoint   = "https://app.harness.io/gateway"
