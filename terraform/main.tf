d0df767cb3fd53cce30a1dbee5243449f9f1371d
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2025-01-30 11:12:07 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,8 +27,8 @@ resource "harness_platform_environment" "someEnv" {
   org_id = data.harness_platform_organization.org.id
   project_id = data.harness_platform_project.project.id
 
-  identifier = "terraformCreatedEnv2"
-  name = "terraformCreatedEnv2"
+  identifier = "terraformCreatedEnv3"
+  name = "terraformCreatedEnv3"
   type = "PreProduction"
 
 }
