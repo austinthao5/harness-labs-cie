493640a4cc2a48c07e6241863d725d5c82c04363
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2025-01-31 14:03:38 -0800

    Update main.tf

--- terragrunt/main.tf
+++ terragrunt/main.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "gcs" {
-    bucket = "austin-test-harness-bucket"  # Replace with your bucket name
-    prefix = "terraform"       # Optional: path inside the bucket to store state
-  }
-}
-
 resource "local_file" "file" {
   content  = "Hello, World!"
   filename = "${path.module}/hi.txt"
