71b89ae93904468844b7304d6d8feb5d0b63964e
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2025-12-18 12:15:32 -0500

    Refactor main method to use message method

--- my-maven-app/src/main/java/com/example/App.java
+++ my-maven-app/src/main/java/com/example/App.java
@@ -1,7 +1,12 @@
 package com.example;
 
 public class App {
+
+    public static String message() {
+        return "Hello from Maven + Docker!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello from Maven + Docker!");
+        System.out.println(message());
     }
 }
